108b8f6f534c52b14a7bb082cfbdf4a3c136fe9b
0qsenxx <semen.antipyuk@gmail.com>
2025-06-03 22:59:39 +0300

    fix styles

--- src/shared/AuthPage/AuthPage.tsx
+++ src/shared/AuthPage/AuthPage.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import clsx from "clsx";
 import { FC } from "react";
 import { AuthPageProps } from "@/entities/AuthPage.type";
+import Image from "next/image";
+import AuthPhoto from "../../../public/authImgs/authPhoto.webp";
+import Container from "../Container/Container";
 
 const AuthPage: FC<AuthPageProps> = ({
   isRegistration,
@@ -11,74 +14,79 @@ const AuthPage: FC<AuthPageProps> = ({
 }) => {
   return (
     <div className={css.authPage}>
-      <div className={css.authBox}>
-        <div className={css.authBlock}>
-          <h2 className={css.authTitle}>
-            {isRegistration ? "Registration" : "Sign In"}
-          </h2>
-          <form className={css.authForm} onSubmit={handleSubmit}>
-            {isRegistration && (
-              <div
-                className={clsx(
-                  css.authNameBefore,
-                  errors.name && css.authErrorAfter
+      <Container>
+        <div className={css.test}>
+          <Image src={AuthPhoto} alt="photo" className={css.authPhoto} />
+          <div className={css.authBox}>
+            <div className={css.authBlock}>
+              <h2 className={css.authTitle}>
+                {isRegistration ? "Registration" : "Sign In"}
+              </h2>
+              <form className={css.authForm} onSubmit={handleSubmit}>
+                {isRegistration && (
+                  <div
+                    className={clsx(
+                      css.authNameBefore,
+                      errors.name && css.authErrorAfter
+                    )}
+                  >
+                    <input
+                      type="text"
+                      className={clsx(
+                        css.authInput,
+                        errors.name && css.authErrorInput
+                      )}
+                      placeholder="Name"
+                      name="name"
+                    />
+                  </div>
                 )}
-              >
-                <input
-                  type="text"
+                <div
                   className={clsx(
-                    css.authInput,
-                    errors.name && css.authErrorInput
+                    css.authEmailBefore,
+                    errors.email && css.authErrorAfter
                   )}
-                  placeholder="Name"
-                  name="name"
-                />
-              </div>
-            )}
-            <div
-              className={clsx(
-                css.authEmailBefore,
-                errors.email && css.authErrorAfter
-              )}
-            >
-              <input
-                type="text"
-                className={clsx(
-                  css.authInput,
-                  errors.email && css.authErrorInput
-                )}
-                placeholder="Email"
-                name="email"
-              />
+                >
+                  <input
+                    type="text"
+                    className={clsx(
+                      css.authInput,
+                      errors.email && css.authErrorInput
+                    )}
+                    placeholder="Email"
+                    name="email"
+                  />
+                </div>
+                <div
+                  className={clsx(
+                    css.authPasswordBefore,
+                    errors.password && css.authErrorAfter
+                  )}
+                >
+                  <input
+                    type="password"
+                    className={clsx(
+                      css.authInput,
+                      errors.password && css.authErrorInput
+                    )}
+                    placeholder="Password"
+                    name="password"
+                  />
+                </div>
+                <button type="submit" className={css.loginBtn}>
+                  Sign In
+                </button>
+              </form>
             </div>
-            <div
-              className={clsx(
-                css.authPasswordBefore,
-                errors.password && css.authErrorAfter
-              )}
+            <Link
+              href={isRegistration ? "/start/login" : "/start/registration"}
+              className={css.authLink}
             >
-              <input
-                type="password"
-                className={clsx(
-                  css.authInput,
-                  errors.password && css.authErrorInput
-                )}
-                placeholder="Password"
-                name="password"
-              />
-            </div>
-            <button type="submit" className={css.loginBtn}>
-              Sign In
-            </button>
-          </form>
+              {isRegistration ? "Sign in" : "Registration"}
+            </Link>
+          </div>
         </div>
-        <Link
-          href={isRegistration ? "/start/login" : "/start/registration"}
-          className={css.authLink}
-        >
-          {isRegistration ? "Sign in" : "Registration"}
-        </Link>
-      </div>
+      </Container>
     </div>
   );
 };
