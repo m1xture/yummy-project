f002592f3ef323e38809126d4bf0bdfa360b859b
m1xture <misha.ivanov.1050@gmail.com>
2025-06-07 10:28:51 +0300

    categories

--- src/redux/apis/categoriesApi.ts
+++ src/redux/apis/categoriesApi.ts
@@ -1,6 +1,7 @@
 import { Category } from "@/entities/Category.type";
 import { baseQueryWithAuth } from "@/features/auth/auth";
 import { createApi } from "@reduxjs/toolkit/query/react";
+import { Recipes, RecipeSmall } from "@/entities/Recipe.type";
 
 export const categoriesApi = createApi({
   reducerPath: "categories",
@@ -9,8 +10,16 @@ export const categoriesApi = createApi({
     getAllCategories: builder.query<Category[], void>({
       query: (): string => `recipes/category/list`,
     }),
+    getCategoryRecipes: builder.query<Recipes, { categoryName: string }>({
+      query: ({ categoryName }): string =>
+        `recipes/category/${
+          categoryName.charAt(0).toUpperCase() + categoryName.slice(1)
+        }`,
+    }),
   }),
 });
 
 export const useGetAllCategories =
   categoriesApi.endpoints.getAllCategories.useQuery;
+export const useGetCategoryRecipes =
+  categoriesApi.endpoints.getCategoryRecipes.useQuery;
--- src/widgets/Categories/Categories.tsx
+++ src/widgets/Categories/Categories.tsx
@@ -1,17 +1,48 @@
 "use client";
 
 import css from "./Categories.module.scss";
-import { useRef, useEffect } from "react";
-import { useSearchParams } from "next/navigation";
+import { useRef, useEffect, useState } from "react";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import Container from "@/shared/Container/Container";
-import { useGetAllCategories } from "@/redux/apis/categoriesApi";
+import {
+  useGetAllCategories,
+  useGetCategoryRecipes,
+} from "@/redux/apis/categoriesApi";
 import { Category } from "@/entities/Category.type";
 import useDraggableScroll from "use-draggable-scroll";
 import clsx from "clsx";
+import RecipeCard from "@/shared/RecipeCard/RecipeCard";
+import { RecipeSmall } from "@/entities/Recipe.type";
 
 const Categories = () => {
-  const param = useSearchParams().get("q");
+  // const param = useSearchParams().get("q");
+  // const [selectedCategory, setSelectedCategory] = useState(
+  //   param ? param : "breakfast"
+  // );
+  // useEffect(() => {}, [selectedCategory]);
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const [selectedCategory, setSelectedCategory] = useState(
+    searchParams.get("q") || "breakfast"
+  );
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+    params.set("q", selectedCategory);
+    router.push(`${pathname}?${params.toString()}`, { scroll: false });
+  }, [selectedCategory]);
+
   const { data, error, isLoading } = useGetAllCategories();
+  const {
+    data: recipeData,
+    error: recipeError,
+    isLoading: recipeIsLoading,
+  } = useGetCategoryRecipes({
+    categoryName: selectedCategory ? selectedCategory : "breakfast",
+  });
   const categoriesListRef = useRef<HTMLUListElement>(null!);
   const { onMouseDown } = useDraggableScroll(categoriesListRef, {
     direction: "horizontal",
@@ -34,10 +65,13 @@ const Categories = () => {
               <li
                 className={clsx(
                   css.categoriesSubitem,
-                  category.title.toLowerCase() === param &&
+                  category.title.toLowerCase() === selectedCategory &&
                     css.categoriesSubitemActive
                 )}
                 id={category._id}
+                onClick={() =>
+                  setSelectedCategory(category.title.toLowerCase())
+                }
                 key={category._id}
               >
                 <p className={css.categoriesName}>{category.title}</p>
@@ -45,7 +79,21 @@ const Categories = () => {
             ))}
           </ul>
         )}
-        <ul className={css.categoriesList}></ul>
+        {recipeIsLoading && <p>Loading...</p>}
+        {recipeError && <p>Error!</p>}
+        {!recipeIsLoading && !recipeError && recipeData && (
+          <div className={css.recipeGrid}>
+            <ul className={css.recipesList}>
+              {recipeData.recipes.map((recipe: RecipeSmall) => (
+                <RecipeCard
+                  title={recipe.title}
+                  imgPath={recipe.preview}
+                  key={recipe._id}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
       </Container>
     </section>
   );
